a91de6071068fcfb96ec467502a1c651fb1d1f69
Laurima104 <gamer11009h@gmail.com>
2026-09-08 12:35:15 -0300

    feat: criando todos os arquivos principais

---
+++ src/br/com/sistema/jdbc/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package br.com.sistema.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection() {
+        try {
+            //configurei a senha para ser puxada pelas variaveis de sistema
+            String senhaBD = System.getenv("pgAdmin");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/bdvendas", "postgres", senhaBD);
+        } catch (SQLException erro) {
+            throw new RuntimeException(erro);
+        }
+    }
+}
---
+++ src/br/com/sistema/main/Main.java
@@ -0,0 +1,16 @@
+package br.com.sistema.main;
+
+import br.com.sistema.view.FrmLogin;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        FrmLogin frmLogin = new FrmLogin();
+        frmLogin.setVisible(true);
+    }
+}
