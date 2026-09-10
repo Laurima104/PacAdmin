0c7f72e56eb0bfd54b7dcb806e64f51b075fabd3
Laurima104 <132705826+Laurima104@users.noreply.github.com>
2026-09-09 21:38:04 -0300

    feat: criando crud em codigo da tb_modulos e iniciando FrmModulo

--- src/br/com/sistema/dao/ModuloDAO.java
+++ src/br/com/sistema/dao/ModuloDAO.java
@@ -1,5 +1,76 @@
 package br.com.sistema.dao;
 
+import br.com.sistema.jdbc.ConnectionFactory;
+import br.com.sistema.model.Modulo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ModuloDAO {
+    static public void salvar(Modulo modulo){
+        String sql = "INSERT INTO tb_modulos(titulo, conteudo) VALUES (?, ?)";
+        
+        try{
+            Connection conn = new ConnectionFactory().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, modulo.getTitulo());
+            stmt.setString(2, modulo.getConteudo());
+            stmt.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
     
+    static public void atualizar(Modulo modulo){
+        String sql = "UPDATE tb_modulos SET titulo = ?, conteudo = ? WHERE id = ?";
+        
+        try {
+            Connection conn = new ConnectionFactory().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, modulo.getTitulo());
+            stmt.setString(2, modulo.getConteudo());
+            stmt.setInt(3, modulo.getId());
+            stmt.executeUpdate();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+    }
+    
+    static public void excluir(int id){
+        String sql = "DELETE FROM tb_modulos WHERE id = ?";
+        
+        try{
+            Connection conn = new ConnectionFactory().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public List<Modulo> listar(){
+        String sql = "SELECT * FROM tb_modulo";
+        
+        List<Modulo> modulos = new ArrayList<>();
+        try{
+            Connection conn = new ConnectionFactory().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Modulo modulo = new Modulo();
+                modulo.setId(rs.getInt("id"));
+                modulo.setTitulo(rs.getString("titulo"));
+                modulo.setConteudo(rs.getString("conteudo"));
+                modulos.add(modulo);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return modulos;
+    } 
 }
--- src/br/com/sistema/view/FrmUsuario.java
+++ src/br/com/sistema/view/FrmUsuario.java
@@ -197,8 +197,8 @@ public class FrmUsuario extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(22, Short.MAX_VALUE)
-                .addComponent(tittleLb)
+                .addContainerGap()
+                .addComponent(tittleLb, javax.swing.GroupLayout.DEFAULT_SIZE, 55, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
